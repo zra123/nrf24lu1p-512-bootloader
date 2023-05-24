71bb92fb2dfd16a0ed587bb48e0a5a005ecee3e1
zra123 <zra123@mail.ru>
2023-05-25 03:52:09 +0700

    Add files via upload

---
+++ scripts/watchman_dongle_enable_bootloader.bat
@@ -0,0 +1,2 @@
+cd %ProgramFiles(x86)%\Steam\steamapps\common\SteamVR\tools\lighthouse\bin\win32
+lighthouse_watchman_update.exe -e ..\..\firmware\vr_controller\archive\htc_pre_vrc_dongle_1450758230_2015_12_22.bin
